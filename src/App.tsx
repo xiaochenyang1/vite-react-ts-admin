8312185a556b8ac9af1788c4234f4b527b19e9cb
肖晨洋 <12274467+xiaochenyang1314@user.noreply.gitee.com>
2024-08-30 18:29:44 +0800

    添加拦截和全局化配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ConfigProvider } from "antd";
 import Login from "@/pages/Login";
 import Home from "@/pages/Home";
 import Layout from "@/pages/Layout";
@@ -7,17 +8,25 @@ import ProtectedRoute from "./ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* 公共路由：登录页面不需要拦截 */}
-        <Route path="/" element={<Login />} />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#1DA57A", // 设置全局主题色
+        },
+      }}
+    >
+      <Router>
+        <Routes>
+          {/* 公共路由：登录页面不需要拦截 */}
+          <Route path="/" element={<Login />} />
 
-        {/* 受保护的路由组 */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Layout" element={<Layout />} />
-        </Route>
-      </Routes>
-    </Router>
+          {/* 受保护的路由组 */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Layout" element={<Layout />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ConfigProvider>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Button } from "antd"; // 引入 Ant Design 的按钮组件
 
 export default function Login() {
-  return <div>登录页</div>;
+  return (
+    <div>
+      登录页
+      <Button type="primary">登录</Button> {/* 这是一个带有主题色的按钮 */}
+    </div>
+  );
 }
